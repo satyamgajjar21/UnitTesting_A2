10c6859619149c48cc702ec4bdcd36a2cad666ac
Satyam <sgajjar2034@conestogac.on.ca>
2022-06-04 00:41:50 -0400

    Completed Rectangle Class

--- Assignment2_Satyam/Program.cs
+++ Assignment2_Satyam/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Assignment2_Satyam
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    
+}
